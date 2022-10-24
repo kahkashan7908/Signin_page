5877bf4f99963ad56e14fc094b9d74ea48a55ad8
kahkashankhatoon <kahkashankhatoon8@gmail.com>
2022-10-24 14:13:04 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var flag = "";
+function ContinueClick() {
+  var userId = document.getElementById("UserId").value;
+  var userMsg1 = document.getElementById("userMsg1").value;
+  var userMsg2 = document.getElementById("userMsg2").value;
+  var regExp1 =/^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+  var regExp2 = /\d{10}/;
+
+  if (userId.match(regExp1)) {
+    document.getElementById("userMsg1").innerHTML = "login success".fontcolor("green");
+    document.getElementById("userMsg2").innerHTML = "";
+    document.getElementById("UserId").value=""
+
+
+  } else {
+    if (userId.match(regExp2)) {
+      document.getElementById("userMsg1").innerHTML = "login success".fontcolor("green");
+      document.getElementById("userMsg2").innerHTML = "";
+      document.getElementById("UserId").value=""
+    } else {
+      document.getElementById("userMsg2").innerHTML ="Invalid Email or Mobile".fontcolor("red");
+      document.getElementById("userMsg1").innerHTML = "";
+    }
+  }
+}
